88958fb47d1b55ae9b2b5f1d0e1ba1c78597fe02
nguyennmt3009 <thaonguyen300996@gmail.com>
2023-04-24 13:59:43 +0900

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div>
+    <h1>{{ num.value }}</h1>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+export default {
+  setup() {
+    const num = ref(0)
+    const increment = () => {
+      num.value++;
+    };
+    return {
+      num,
+      increment,
+    };
+  },
+  name: 'Test',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
 Vue.config.productionTip = false
+
+
+import VueCompositionApi from '@vue/composition-api';
+Vue.use(VueCompositionApi);
 
 new Vue({
   render: h => h(App),
