695f9c4c42b811f1eab50b3be4cf855f547f5524
nguyennmt3009 <thaonguyen300996@gmail.com>
2023-04-24 14:46:46 +0900

    api

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,22 +1,57 @@
 
 <template>
   <div>
+    <h1>Fade Chat GPT</h1>
+    <input class="big-form" v-model="message.value" type="text" placeholder="Enter your question">
+    <input type="submit" @click="sendMessage" value="Send">
+    <!-- show loading -->
+    <h2 v-if="isLoading.value">Loading...</h2>
+    <h1>{{ 'Answer' }}</h1>
+    <p>{{ answer.value }}<p>
+    <br>
     <h1>{{ num.value }}</h1>
     <button @click="increment">Increment</button>
+    <br>
+    <img :src="imageURL.value" alt="random image">
+    <br>
+    <button @click="randomImage">Get new random image</button>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/composition-api';
+import { getChatbot } from '@/api/chatbot';
+
+
 export default {
   setup() {
-    const num = ref(0)
+    const num = ref(0);
+    const isLoading = ref(false);
+    const imageURL = ref('https://picsum.photos/200/300');
+    const message = ref('');
+    const answer = ref('');
     const increment = () => {
       num.value++;
+    };
+    const randomImage = () => {
+      imageURL.value = `https://picsum.photos/200/300?random=${Math.random()}`;
+    };
+    const sendMessage = async () => {
+      isLoading.value = true;
+      console.log(message.value);
+      const response = await getChatbot(message.value);
+      answer.value = response.data.choices[0].text;
+      isLoading.value = false;
     };
     return {
       num,
       increment,
+      imageURL,
+      randomImage,
+      message,
+      sendMessage,
+      answer,
+      isLoading,
     };
   },
   name: 'Test',
@@ -38,5 +73,35 @@ li {
 }
 a {
   color: #42b983;
+}
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+p {
+  white-space: pre-line;
+  text-align: left;
+  padding: 20px;
 }
 </style>
