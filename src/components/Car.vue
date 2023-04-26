3419927c25fbc3fca605c8cdca09ab5ea9aa95f5
nguyennmt3009 <thaonguyen300996@gmail.com>
2023-04-26 09:16:51 +0900

    fix vue

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -13,19 +13,15 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive } from '@vue/composition-api';
 import { Car } from '@/models/Car';
-
 
 export default {
   setup() {
-    const car = ref(new Car({ brand: 'BMW', model: 'M3', price: 100000}));
-    let car2 = 12;
+    const car = reactive(new Car({ brand: 'BMW', model: 'M3', price: 100000}));
 
-    car2++;
     return {
       car,
-      car2,
     };
   },
   name: 'Car',
